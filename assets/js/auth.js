59b5ad6efe1fbe24c306afa3a08df7c5e83c285d
Rohith Krishna <roal2027@gmail.com>
2024-04-02 09:23:01 +0530

    redirection link changed

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -2,7 +2,7 @@
 
         // Check if access token exists
         if (!accessToken) {
-            window.location.href = 'http://127.0.0.1:5508/dist/login.html'; // Redirect to login page
+            window.location.href = 'https://furnitureemporium.shop/login.html'; // Redirect to login page
         } else {
             console.log('Access token:', accessToken);
         }
